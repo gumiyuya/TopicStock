e99bee8a9e84bb10e54d65379aad6a2f81e9abcc
{tamamushi} <{tamamushich@outlook.jp}>
2024-03-31 06:08:13 +0900

    cssを適切なフォルダに移動、body内でのturboの無効化によるJavaScriptの適正化

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,6 +1,18 @@
 class Connection < ApplicationRecord
   validates :topic_id, { presence: true, uniqueness: { scope: :similar_topic_id } }
-  belongs_to :topic
   validates :similar_topic_id, { presence: true, uniqueness: { scope: :topic_id } }
+  belongs_to :topic
   belongs_to :similar_topic
+
+  validate :validate_connections_count, on: :create
+
+  private
+
+  def validate_connections_count
+    connections_count1 = Connection.where(topic_id: self.topic_id).count
+    connections_count2 = Connection.where(similar_topic_id: self.similar_topic_id).count
+    if connections_count1 >= 6 || connections_count2 >= 6
+      errors.add(:base, "1つのトピックに紐づけられるトピックは6つまでです")
+    end
+  end
 end
