473eb2dfbe54eb24afff077896e2fb4602c44c7a
{tamamushi} <{tamamushich@outlook.jp}>
2024-04-10 06:10:09 +0900

    検索フォームの実装、sassの微調整

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,14 @@
 class SearchesController < ApplicationController
+
+  def search
+    @keyword = params[:keyword]
+    @topics = Topic.where(
+      "content LIKE? AND user_id = ?",
+      "%#{@keyword}%", @current_user.id
+    )
+    if @topics.count == 0
+      @error_message = "トピックが見つかりませんでした"
+    end
+  end
+
 end
