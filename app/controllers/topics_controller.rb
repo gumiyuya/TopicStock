d90a30cdfd9c3ba4fdf5a457b3d4ef8c78bb6c3c
{tamamushi} <{tamamushich@outlook.jp}>
2024-04-30 02:02:57 +0900

    トップページとメインページの完成（アプリ完成）

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,14 @@
 class TopicsController < ApplicationController
   
   def stock
+    @topic = Topic.find_by(id: params[:topic_id])
+    if @topic
+      @connections = Connection.where(topic_id: @topic.id)
+    else
+      topics = Topic.where(user_id: 1)
+      @topic = topics[rand(topics.size)]
+      @connections = Connection.where(topic_id: @topic.id)
+    end
   end
 
 end
