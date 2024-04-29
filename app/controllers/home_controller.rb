0e531a4750f3b5d4b6a7206538dc88878a6ebc4a
{tamamushi} <{tamamushich@outlook.jp}>
2024-04-30 02:08:57 +0900

    ログイン状態でトップページにアクセスできないよう制限

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_action :ensure_correct_user_by_user_id
+
   def top
+  end
+
+  # アクセス制限
+  def ensure_correct_user_by_user_id
+    if @current_user
+      session[:user_id] = nil
+    end
   end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
-  
+  before_action :ensure_correct_user_by_user_id
+
   def stock
     @topic = Topic.find_by(id: params[:topic_id])
     if @topic
@@ -8,6 +9,14 @@ class TopicsController < ApplicationController
       topics = Topic.where(user_id: 1)
       @topic = topics[rand(topics.size)]
       @connections = Connection.where(topic_id: @topic.id)
+    end
+  end
+
+  # アクセス制限
+  def ensure_correct_user_by_user_id
+    if @current_user
+      session[:user_id] = nil
+      redirect_to("/TopicStock")
     end
   end
 
